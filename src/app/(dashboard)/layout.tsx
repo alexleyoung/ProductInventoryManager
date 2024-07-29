6a64ed2f7e7544c6d4ac4c090002cb0e7a8a7668
alx-yng <alexyoung0119@gmail.com>
2024-07-29 14:18:33 -0500

    dev: start sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,12 @@
+import Sidebar from "@/components/dashboard/Sidebar";
+
 const DashLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <>
+      <Sidebar />
+      <main className='ml-[4.3rem]'>{children}</main>
+    </>
+  );
 };
 
 export default DashLayout;
---
+++ src/components/dashboard/Sidebar.tsx
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+  return (
+    <aside className='border-r border-primary-border w-[4.3rem] h-screen fixed flex flex-col justify-between items-center py-4'>
+      <div>test</div>
+      <div>test</div>
+    </aside>
+  );
+};
+
+export default Sidebar;
